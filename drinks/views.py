5354fbddc4d5336b2e3e3af38100e62f1cc2895c
Munachi <victorycollins31@gmail.com>
2023-05-20 13:03:57 +0100

    creatd the  views.py

---
+++ drinks/views.py
@@ -0,0 +1,10 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
+def drink_list(request):
+    # get all the drinks
+    drinks= Drink.objects.all()
+    # instance of the DrinkSerializer class
+    serializer= DrinkSerializer(drinks, many=True)
+    return JsonResponse(serializer.data)
+
