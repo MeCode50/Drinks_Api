c4b05e127d55ad3e3fe5cc0d139d54ea1c2e1e94
Munachi <victorycollins31@gmail.com>
2023-05-20 13:30:11 +0100

    created urls.py and path drinks

--- drinks/views.py
+++ drinks/views.py
@@ -6,5 +6,5 @@ def drink_list(request):
     drinks= Drink.objects.all()
     # instance of the DrinkSerializer class
     serializer= DrinkSerializer(drinks, many=True)
-    return JsonResponse(serializer.data)
+    return JsonResponse({"drinks":serializer.data}, safe=False)
 
